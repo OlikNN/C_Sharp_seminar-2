ce7d36df01721749fea67d6834802ab27a16fee6
OlikNN <oliknn@gmail.com>
2022-11-30 21:28:23 +0300

    text

---
+++ task12/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая будет принимать на вход два числа и выводить,
+// является ли второе число кратным первому. Если число 2 не кратно числу 1, 
+// то программа выводит остаток от деления
+
+// 34, 5 -> не кратно, остаток 4
+// 16, 4 -> кратно 
+
+Console.WriteLine("Введите числ A");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите числ B");
+int numberB = Convert.ToInt32(Console.ReadLine());
+int ostatoc = numberA % numberB;
+
+if (ostatoc > 0)
+{
+    Console.WriteLine($"{numberA}, {numberB} -> не кратно, остаток {ostatoc}");
+}
+else
+{
+    Console.WriteLine($"{numberA}, {numberB} -> кратно");
+}
---
+++ task16/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая принимает на вход два числа и проверяет, 
+// является ли одно число квадратом другого.
+// 5, 25 -> да
+// -4, 16 -> да
+// 25, 5 -> да
+// 8, 9 -> нет
+
+Console.WriteLine("Введите первое числ");
+int digit1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе числ");
+int digit2 = Convert.ToInt32(Console.ReadLine());
+
+if (digit2 == digit1 * digit1 || digit1 == digit2 * digit2)
+{
+    Console.WriteLine("да");
+}
+else
+{
+    Console.WriteLine("нет");
+}
